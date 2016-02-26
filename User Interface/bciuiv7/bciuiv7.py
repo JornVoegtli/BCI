1ace11cea24edaa474db80cc8c02550614bdc409
Aaron Low <awl14@ic.ac.uk>
2016-02-26 13:17:47 +0000

    Added files via upload

--- User Interface/bciuiv7/bciuiv7.py
+++ User Interface/bciuiv7/bciuiv7.py
@@ -1,13 +1,14 @@
+from __future__ import division, print_function, unicode_literals
 import os, sys
-import pyglet
 from pyglet.gl import *
 from pyglet import *
 from pyglet.window import *
-import ctypes
 import primitives
+import user_input
 import random
 import string
-sys.path.append(r'C:\Users\Jun Shern\Dropbox\EE2 BCI Project\User Interface\bciuiv7')
+import ctypes
+#sys.path.append(r'C:\Users\AARON\Dropbox\EE2 BCI Project\User Interface\bciuiv7')
 
 class MyOVBox(OVBox):
     def __init__(self):
@@ -20,28 +21,36 @@ class MyOVBox(OVBox):
         self.textstring = ""
         self.textmode = 1
         self.keepWriting = 1 
-        # Pygame
-        win = window.Window()
-        glClearColor(.8, .8, .8, 1.0)
+        # Pyglet
+        self.win = window.Window(fullscreen = True)
+        glClearColor(0, 0, 1, 1.0)
         glEnable(GL_BLEND)
-        keys = key.KeyStateHandler()
-        win.push_handlers(keys)
+        #pyglet.clock.set_fps_limit(12)
+        self.keys = key.KeyStateHandler()
+        self.win.push_handlers(self.keys)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        p = primitives.Pixel(10,10)
-        c = primitives.Circle(200,100,width=100,color=(0.,.9,0.,1.))
-        a = primitives.Arc(150,150,radius=100,color=(1.,0.,0.,1.),sweep=90,style=GLU_FILL)
-        P = primitives.Polygon([(0, 0), (50, 200), (80, 200),(60,100),(100,5)],color=(.3,0.2,0.5,.7))
-        l = primitives.Line((10,299),(100,299),stroke=2,color=(0,0.,1.,1.))
-        label = pyglet.text.Label("Hello, world!", 
-            font_name='Times New Roman',
+        user32 = ctypes.windll.user32
+        self.width = user32.GetSystemMetrics(0)
+        self.height = user32.GetSystemMetrics(1)
+        self.words = pyglet.text.Label("Y              Z              SPACE             \u25C4              \u25BC              \u25BA", 
+            font_name='monospace',
             font_size=36,
-            color=(0,0,0,255),
-            x=win.width//2, y=win.height//2,
-            anchor_x='center', anchor_y='center')
+            color=(255,255,255,255),
+            x=0, y=0,
+            anchor_x='left', anchor_y='bottom')
+        self.batch = pyglet.graphics.Batch()
+        self.widget = user_input.TextWidget('', 0, int(self.height/2), int(self.width),int(self.height/2), self.batch)
+        text = "hi sdf sdf sd fsd fsd f sdf sd fds f dsf sdf ds fds fds f dsf ds fds fsd f sd s dfs df sdf ds fsd f ds fds fs dfdsf ds f sdf dsf ds fsd fsd f sdf sd fsddf ds"
+        self.widget.caret.on_text(text)
+
+        self.win.dispatch_events()
+        self.win.flip()
+   
         return
             
     def process(self): # Called on each box clock tick (this can be configured by right-clicking the box)
         #print "fps:", clock.get_fps()
+        #clock.tick()
         self.count += 1
         self.count = self.count % 20
         if (self.count == 0):
@@ -57,21 +66,28 @@ class MyOVBox(OVBox):
                 #    print target_stim.identifier
                 self.target[0] = random.randint(0,5)
                 self.target[1] = random.randint(0,5)
-        word_display.displayUpdate(self.target,self.keepWriting,self.textstring, self.textmode, self.label)
+        #word_display.displayUpdate(self.target,self.keepWriting,self.textstring, self.textmode, self.label
+        if self.win.has_exit:
+            self.win.close()
+            sys.exit()
 
-        win.dispatch_events()
-        col = (255,0,0,1)
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        c.color = col
-        if(keys[key.A]):
-            c.render()
-        p.render()
-        a.render()
-        a.rotation+=1
-        P.render()
-        l.render()
-        label.draw()
-        win.flip()
+        else:
+            self.label2 = random.randint(0,11)
+            self.target0 = random.randint(0,5)
+            self.target1 = random.randint(0,5)
+            self.win.dispatch_events()
+            if(self.keys[key.A]):
+                self.win.close()
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+            primitives.drawRect(0,self.height/2,self.width,self.height/2,4/51,4/51,4/51,255)
+            self.batch.draw() 
+            if self.label2 < 6:
+                primitives.drawRect(self.label2*self.width/6,0,self.width/6,self.height/2,1,1,0,1)
+            elif self.label2 < 12:
+                primitives.drawRect(0,self.label2%6*self.height/12,self.width,self.height/12,1,1,0,1)
+            primitives.drawRect(self.target0*self.width/6,self.target1*self.height/12,self.width/6,self.height/12,0,1,0,1)
+            self.words.draw()
+            self.win.flip()
         return
     
  
@@ -85,8 +101,7 @@ class MyOVBox(OVBox):
         return None
             
     def uninitialize(self): # Called once when stopping the scenario
-        pygame.display.quit()
-        pygame.quit()
+        self.win.close()
         sys.exit()
         return
 
--- User Interface/bciuiv7/user_input.py
+++ User Interface/bciuiv7/user_input.py
@@ -20,26 +20,22 @@ class Rectangle(object):
         )
 
 class TextWidget(object):
-    def __init__(self, text, x, y, width, batch):
+    def __init__(self, text, x, y, width,height, batch):
         self.document = pyglet.text.document.UnformattedDocument(text)
         self.document.set_style(0, len(self.document.text), 
             dict(color=(0, 0, 0, 255))
         )
-        #font = self.document.get_font()
-        #height = font.ascent - font.descent
-        #self.layout = pyglet.text.layout.IncrementalTextLayout(
-        #    self.document, width, height, multiline=False, batch=batch)
-        #self.caret = pyglet.text.caret.Caret(self.layout)
+        font = self.document.get_font()
+        self.layout = pyglet.text.layout.IncrementalTextLayout(
+            self.document, width, height, multiline=False, batch=batch)
+        self.caret = pyglet.text.caret.Caret(self.layout)
 
-        #self.layout.x = x
-        #self.layout.y = y
+        self.layout.x = x
+        self.layout.y = y
 
-        # Rectangular outline
-        #pad = 2
-        #self.rectangle = Rectangle(x - pad, y - pad, 
-        #                           x + width + pad, y + height + pad, batch)
+        #Rectangular outline
+        pad = 2
+        self.rectangle = Rectangle(x - pad, y - pad, 
+                                   x + width + pad, y + height + pad, batch)
 
-    #def hit_test(self, x, y):
-    #    return (0 < x - self.layout.x < self.layout.width and
-    #            0 < y - self.layout.y < self.layout.height)
-
+    
